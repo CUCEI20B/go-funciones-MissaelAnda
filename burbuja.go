096e03b5abc1e07b3f000277598dee0fc6212c61
repl.it user <replituser@example.com>
2020-10-18 20:51:59 +0000

    Hecho

--- burbuja.go
+++ burbuja.go
@@ -1,7 +1,21 @@
 package main
 
 func Burbuja(s []int64)  {
+  var aux int64
+	var i int = 0
+	for i < len(s)-1 {
+		if s[i+1] < s[i] {
+			aux = s[i]
+			s[i] = s[i+1]
+			s[i+1] = aux
 
+			if i > 0 {
+				i--
+			}
+		} else {
+			i++
+		}
+	}
 }
 
 func main()  {
